6fb46745b1d44af7e697eb8ecc01d7a360af8a62
andrewreza <arlarty@gmail.com>
2021-09-12 21:08:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var today = moment().format('LLL');
+$("#currentDay").text(today);
+
+var localTime = moment().format('HH');
+console.log(localTime)
+
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+
+    var userInfo = $(this).prev().val();
+    console.log(userInfo)
+    localStorage.setItem($(this).prev().attr("id"), userInfo)
+
+});
+
+   $("input").each(function () {
+        var selectedInput = parseInt($(this).attr("id"));
+
+        if (selectedInput > localTime) {
+            $(this).addClass("future")
+
+        } else if (selectedInput == localTime) {
+
+            $(this).addClass("present")
+
+        }
+        else {
+            $(this).addClass("past")
+
+        };
+
+        $("#" + selectedInput).val(localStorage.getItem(selectedInput))
+    });
